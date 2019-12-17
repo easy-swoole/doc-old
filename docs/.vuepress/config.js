a9967d7c2aa831f147d7dab26b9ae2a01881b7ae
如果的如果 <admin@fosuss.com>
2019-12-17 13:03:59 +0800

    百度统计代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,6 +41,7 @@ module.exports = {
     },
     head: [
         ['script', {src: 'https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js'}],
+        ['script', {src: '/bdtj.js'}],
         ['script', {src: 'https://cdn.bootcss.com/layer/2.3/layer.js'}],
         ['href', {src: 'https://cdn.bootcss.com/layer/2.3/skin/layer.css', rel: "stylesheet"}],
         ['link', {rel: "icon", type: "image/x-icon", href: "/favicon.ico"}]
