3eb1b7a094ab4f10dadce28c8ae1295dcb36735c
如果的如果 <admin@fosuss.com>
2019-12-28 20:02:57 +0800

    up fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ module.exports = {
     locales: {
         '/': {
             lang: 'zh-CN',
-            title: 'EasySwoole，Swoole框架,一款基于swoole实现的高并发与开发效率共存的微服务分布式框架',
-            description: 'easySwoole，swoole框架是一款高度封装了swoole拓展而依旧保持swoole原有特性的一个高性能分布式微服务框架，旨在提供一个高效、快速、优雅的框架给php开发者'
+            title: 'swoole,easyswoole,swoole框架,一款基于swoole实现的高并发与开发效率共存的微服务分布式框架',
+            description: 'swoole,easyswoole,swoole框架是一款高度封装了swoole拓展而依旧保持swoole原有特性的一个高性能分布式微服务框架，旨在提供一个高效、快速、优雅的框架给php开发者'
         },
         '/En/': {
             lang: 'en-US',
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,7 @@
 ---
 meta:
   - name: keywords
-    content: easySwoole,swoole,swoole框架,swoole拓展,swoole微服务,swoole分布式,swoole rpc,swoole websocket
+    content: swoole,easyswoole,swoole框架,swoole拓展,swoole微服务,swoole分布式,swoole websocket
 home: true
 heroSvg: /resources/hero.svg
 heroImage: /resources/hero.png
